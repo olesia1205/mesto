34311a166e737c3c80cb2a504b340e39ba037a05
olesia1205 <olesia1205@yandex.ru>
2022-10-18 09:53:45 +0300

    Added like-button-status

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,49 @@
+// Открытие и закрытие модального окна
 const popupOpenButtonElement = document.querySelector('.profile__info-edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 
-popupElement.classList.toggle('popup_is-opened');
+const openPopup = function(event) {
+  popupElement.classList.add('popup_is-opened');
+}
+
+const closePopup = function() {
+  popupElement.classList.remove('popup_is-opened');
+}
+
+const closePopupByClickOnOverlay = function(event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+  closePopup();
+}
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+
+//Редактирование имени и информации о себе
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__container');
+
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__info');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
