e14aeedf1d4de379f1b657a029d03f08894be495
olesia1205 <olesia1205@yandex.ru>
2022-10-26 19:15:25 +0300

    Added popupCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-// Открытие и закрытие модального окна
+// Открытие, закрытие попапа редактирования профиля пользователя - переменные //
 const popupOpenButtonElement = document.querySelector('.profile__info-edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
@@ -11,6 +11,13 @@ const profilElement = document.querySelector('.profile');
 const nameProfil = profilElement.querySelector('.profile__info-title');
 const jobProfil = profilElement.querySelector('.profile__info-subtitle');
 
+// Открытие, закрытие попапа редактирования карточек - переменные //
+const popupCardOpenButtonElement = document.querySelector('.profile__add-button');
+const popupCardElement = document.querySelector('.popup_type_card');
+const popupCardCloseButtonElement = popupCardElement.querySelector('.popup__close-button');
+
+
+// Открытие, закрытие попапа редактирования профиля пользователя - функции //
 const openPopup = function() {
   popupElement.classList.add('popup_is-opened');
   nameInput.value = nameProfil.textContent;
@@ -28,10 +35,22 @@ function addInfo(evt) {
   closePopup();
 }
 
+// Открытие, закрытие попапа редактирования карточек - функции//
+const openPopupCard = function() {
+  popupCardElement.classList.add('popup_is-opened');
+}
+
+const closePopupCard = function() {
+  popupCardElement.classList.remove('popup_is-opened');
+}
+
+// Открытие, закрытие попапа редактирования профиля пользователя - слушатели событий //
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', addInfo);
 
+popupCardOpenButtonElement.addEventListener('click', openPopupCard);
+popupCardCloseButtonElement.addEventListener('click', closePopupCard);
 // const closePopupByClickOnOverlay = function(event) {
 //   if (event.target !== event.currentTarget) {
 //     return;
