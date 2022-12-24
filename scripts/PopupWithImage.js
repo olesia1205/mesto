043f770ac1f055d830a0bb025aa9f3e026449177
olesia1205 <olesia1205@yandex.ru>
2022-12-24 12:54:28 +0300

    добавила класс PopupWithImage

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,16 @@
+import Popup from './Popup.js';
+
+class PopupWithImage extends Popup {
+  constructor(popupSelector, popupImage, popupImageSubtitle) {
+    super(popupSelector);
+    this._popupImage = popupImage;
+    this._popupImageSubtitle = popupImageSubtitle;
+  }
+
+  openPopup() {
+    super.openPopup(text, link);
+    this._popupImageSubtitle.textContent = text;
+    this._popupImage.src = link;
+    this._popupImage.alt = text;
+  }
+}
