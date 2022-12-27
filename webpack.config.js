fdd96744d6925bad3a884b1c4e27a38787f06bfe
olesia1205 <olesia1205@yandex.ru>
2022-12-27 09:18:42 +0300

    минификация css

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,11 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader'
-          }]
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          'postcss-loader'
+        ]
       }
     ]
   },
